eb6f47b0db26d121de3ec68ef6023443c1a9d0ab
dilshansr <150016010+dilshansr@users.noreply.github.com>
2024-05-16 15:59:10 +0530

    Delete my_streamlit_app directory

--- my_streamlit_app/pages/page1.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-def show(name):
-    st.title("Page 1")
-    name_input = st.text_input("Your name:", value=name)
-    st.write(f"Hello, {name}, you are on Page 1.")
-    if st.button("Update Name"):
-        st.experimental_set_query_params(page='page1', name=name_input)
-    st.markdown("[Go Home](?page=home&name={})".format(name_input))
--- my_streamlit_app/pages/page2.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-def show(name):
-    st.title("Page 2")
-    name_input = st.text_input("Your name:", value=name)
-    st.write(f"Hello, {name}, you are on Page 2.")
-    if st.button("Update Name"):
-        st.experimental_set_query_params(page='page2', name=name_input)
-    st.markdown("[Go Home](?page=home&name={})".format(name_input))
